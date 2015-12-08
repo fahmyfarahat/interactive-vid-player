a8664dc12cf7479706ffe2dc3c37f030cdde6f70
Fahmy Farahat <fahmy.farahat@gmail.com>
2015-12-09 01:31:24 +0200

    [ADD] init videogular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,7 +11,11 @@
 angular
   .module('koraPlayerApp', [
     'ngRoute',
-    'ngSanitize'
+    'ngSanitize',
+    "com.2fdevs.videogular",
+    "com.2fdevs.videogular.plugins.controls",
+    "com.2fdevs.videogular.plugins.overlayplay",
+    "com.2fdevs.videogular.plugins.poster"
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,31 @@
  * # MainCtrl
  * Controller of the koraPlayerApp
  */
+
+(function(){
+    function controller (scope , $sce){
+        scope.config = {
+            sources: [
+                {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.mp4"), type: "video/mp4"},
+                {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.webm"), type: "video/webm"},
+                {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.ogg"), type: "video/ogg"}
+            ],
+            tracks: [
+                {
+                    src: "http://www.videogular.com/assets/subs/pale-blue-dot.vtt",
+                    kind: "subtitles",
+                    srclang: "en",
+                    label: "English",
+                    default: ""
+                }
+            ],
+            theme: "bower_components/videogular-themes-default/videogular.css",
+            plugins: {
+                poster: "http://www.videogular.com/assets/images/videogular.png"
+            }
+        };
+    }
+
 angular.module('koraPlayerApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', ['$scope','$sce', controller]);
+})();
