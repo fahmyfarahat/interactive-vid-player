4049001301738739cd2a98dcf1b5380723a3e83c
Fahmy Farahat <fahmy.farahat@gmail.com>
2015-12-09 00:37:19 +0200

    init angular project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name koraPlayerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the koraPlayerApp
+ */
+angular.module('koraPlayerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name koraPlayerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the koraPlayerApp
+ */
+angular.module('koraPlayerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
